a70b0a70ae96dcf398ca6da33794424d08bef28c
yyytakahashi <yuta01072002@icloud.com>
2025-07-27 00:47:30 +0900

    Updates

---
+++ assets/BootFormsView-k3XUedfr.js
@@ -0,0 +1,111 @@
+import{_ as N,h as t,c as n,a,b as E,e as c,d as o,m as r,w as V,r as F,t as s,k as O,l as B,F as A,o as d}from"./index-aeD5elKR.js";import{F as S}from"./FooterParts-0nRMGmGh.js";const z={class:"bootforms mb-2"},D={class:"row"},H={class:"col-lg-8 mx-auto"},I={class:"d-flex gap-3"},R={class:"mt-4"},T={key:0,class:"bg-dark text-white p-3 rounded"},Y={class:"bootforms mb-2"},L={class:"row"},M={class:"col-lg-8 mx-auto"},P={class:"mt-4"},U={key:0,class:"bg-dark text-white p-3 rounded"},j={class:"bootforms mb-2"},q={class:"row"},G={class:"col-lg-8 mx-auto"},J={class:"mt-4"},K={key:0,class:"bg-dark text-white p-3 rounded"},Q={class:"bootforms mb-2"},W={class:"row"},X={class:"col-lg-8 mx-auto"},Z={class:"mt-4"},ee={key:0,class:"bg-dark text-white p-3 rounded"},ae={class:"bootforms mb-2"},oe={class:"row"},se={class:"col-lg-8 mx-auto"},le={class:"mt-4"},te={key:0,class:"bg-dark text-white p-3 rounded"},ne={class:"bootforms mb-2"},de={class:"row"},ie={class:"col-lg-8 mx-auto"},ce={class:"mt-4"},re={key:0,class:"bg-dark text-white p-3 rounded"},me={class:"bootforms mb-2"},be={class:"row"},ve={class:"col-lg-8 mx-auto"},ue={class:"mb-2"},fe={class:"mt-3"},pe={key:0,class:"bg-dark text-white p-3 rounded"},ke={class:"bootforms mb-2"},he={class:"row"},ye={class:"col-lg-8 mx-auto"},xe={class:"mt-3"},ge={key:0,class:"bg-dark text-white p-3 rounded"},_e={class:"bootforms mb-2"},we={class:"row"},Ce={class:"col-lg-8 mx-auto"},Ee={class:"mt-3"},$e={key:0,class:"bg-dark text-white p-3 rounded"},Ne={class:"bootforms mb-2"},Ve={class:"row"},Fe={class:"col-lg-8 mx-auto"},Oe={class:"mt-3"},Be={key:0,class:"bg-dark text-white p-3 rounded"},Ae={class:"bootforms mb-2"},Se={class:"row"},ze={class:"col-lg-8 mx-auto"},De={class:"mt-3"},He={key:0,class:"bg-dark text-white p-3 rounded"},Ie={__name:"BootFormsView",setup(Re){const m=t(!1),b=t(!1),v=t(!1),u=t(!1),f=t(!1),p=t(!1),k=t(!1),h=t(!1),y=t(!1),x=t(!1),g=t(!1),w=t(50),_=t(!0);function i(C){switch(C){case 1:m.value=!m.value;break;case 2:b.value=!b.value;break;case 3:v.value=!v.value;break;case 4:u.value=!u.value;break;case 5:f.value=!f.value;break;case 6:p.value=!p.value;break;case 7:k.value=!k.value;break;case 8:h.value=!h.value;break;case 9:y.value=!y.value;break;case 10:x.value=!x.value;break;case 11:g.value=!g.value;break}}return(C,e)=>{const $=F("router-link");return d(),n(A,null,[e[52]||(e[52]=a("h1",{class:"mb-2 ms-3"},"フォーム（Forms）",-1)),a("div",z,[a("div",D,[a("div",H,[e[18]||(e[18]=a("h2",{class:"mb-2"},"フォーム（Forms）",-1)),e[19]||(e[19]=a("p",{class:""},[o(" フォームは、ラベルに "),a("code",null,".form-label"),o(" を、"),a("code",null,"<input> "),o(" 等のコントロールに "),a("code",null,".form-control"),o(" を指定します。 ")],-1)),a("form",null,[e[15]||(e[15]=r('<div class="mb-2" data-v-05b676e4><label class="form-label" for="name" data-v-05b676e4>名前</label><input class="form-control" type="text" name="name" id="name" data-v-05b676e4></div><div class="mb-2" data-v-05b676e4><label class="form-label" for="address" data-v-05b676e4>メールアドレス</label><input class="form-control" type="text" name="address" id="address" data-v-05b676e4></div>',2)),a("div",I,[e[14]||(e[14]=a("button",{type:"reset",class:"btn btn-secondary"},"リセット",-1)),E($,{to:"/bootmenu",class:"btn btn-primary button"},{default:V(()=>e[13]||(e[13]=[o(" 登録 ")])),_:1,__:[13]})])]),a("h5",R,[e[16]||(e[16]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[0]||(e[0]=l=>i(1))},s(m.value?"非表示":"表示"),1)]),m.value?(d(),n("pre",T,e[17]||(e[17]=[a("code",null,`<form>
+  <div class="mb-2">
+    <label class="form-label" for="name">名前</label>
+    <input class="form-control" type="text" name="name" id="name">
+  </div>
+  <div class="mb-2">
+    <label class="form-label" for="address">メールアドレス</label>
+    <input class="form-control" type="text" name="address" id="address">
+  </div>
+  <button type="reset" class="btn btn-secondary">リセット</button>
+  <button type="submit" class="btn btn-primary">登録</button>
+</form>`,-1)]))):c("",!0)])])]),a("div",Y,[a("div",L,[a("div",M,[e[22]||(e[22]=r('<h2 class="mb-2" data-v-05b676e4>フォームテキスト(Form text)</h2><p class="" data-v-05b676e4><code data-v-05b676e4>.form-text</code> は、フォームの注釈分などのテキストを表示します。</p><form data-v-05b676e4><div class="mb-2" data-v-05b676e4><input class="form-control" type="text" name="name" data-v-05b676e4><div class="form-text" data-v-05b676e4>全角文字で入力してください</div></div></form>',3)),a("h5",P,[e[20]||(e[20]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[1]||(e[1]=l=>i(2))},s(b.value?"非表示":"表示"),1)]),b.value?(d(),n("pre",U,e[21]||(e[21]=[a("code",null,`<input class="form-control" type="text" name="name">
+  <div class="form-text" for="name">全角文字で入力してください</div>
+`,-1)]))):c("",!0)])])]),a("div",j,[a("div",q,[a("div",G,[e[25]||(e[25]=r('<h2 class="mb-2" data-v-05b676e4>サイジング(Sizing)</h2><p class="" data-v-05b676e4><code data-v-05b676e4>.form-control-lg</code> は大きなフォーム、<code data-v-05b676e4>.form-control-sm</code> は小さなフォームを表示します。 </p><div class="mb-2" data-v-05b676e4><input class="form-control form-control-sm mb-1" type="text" data-v-05b676e4><input class="form-control mb-1" type="text" data-v-05b676e4><input class="form-control form-control-lg" type="text" data-v-05b676e4></div>',3)),a("h5",J,[e[23]||(e[23]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[2]||(e[2]=l=>i(3))},s(v.value?"非表示":"表示"),1)]),v.value?(d(),n("pre",K,e[24]||(e[24]=[a("code",null,`<input class="form-control form-control-sm" type="text" />
+<input class="form-control" type="text" />
+<input class="form-control form-control-lg" type="text" />
+`,-1)]))):c("",!0)])])]),a("div",Q,[a("div",W,[a("div",X,[e[28]||(e[28]=r('<h2 class="mb-2" data-v-05b676e4>チェックボックス＆ラジオボタン(Checks &amp; Radios)</h2><p class="" data-v-05b676e4> チェックボックスやラジオボタンには<code data-v-05b676e4>.form-check</code>,<code data-v-05b676e4>.form-check-input</code>,<code data-v-05b676e4>.form-check-label</code> を指定します。 </p><div class="form-check" data-v-05b676e4><input class="form-check-input" type="checkbox" id="check_1" name="agree" value="yes" data-v-05b676e4><label class="form-check-label" for="check_1" data-v-05b676e4>規約に同意します</label></div><div class="form-check" data-v-05b676e4><input class="form-check-input" type="radio" id="radio_1" name="gender" value="male" checked data-v-05b676e4><label class="form-check-label" for="radio_1" data-v-05b676e4>男性</label></div><div class="form-check" data-v-05b676e4><input class="form-check-input" type="radio" id="radio_2" name="gender" value="female" data-v-05b676e4><label class="form-check-label" for="radio_2" data-v-05b676e4>女性</label></div>',5)),a("h5",Z,[e[26]||(e[26]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[3]||(e[3]=l=>i(4))},s(u.value?"非表示":"表示"),1)]),u.value?(d(),n("pre",ee,e[27]||(e[27]=[a("code",null,`<div class="form-check">
+  <input class="form-check-input" type="checkbox" id="check_1" name="agree" value="yes" />
+  <label class="form-check-label" for="check_1">規約に同意します</label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" id="radio_1" name="gender" value="male" checked />
+  <label class="form-check-label" for="radio_1">男性</label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" id="radio_2" name="gender" value="female" />
+  <label class="form-check-label" for="radio_2">女性</label>
+</div>
+`,-1)]))):c("",!0)])])]),a("div",ae,[a("div",oe,[a("div",se,[e[31]||(e[31]=r('<h2 class="mb-2" data-v-05b676e4>スイッチ(Switches)</h2><p class="" data-v-05b676e4><code data-v-05b676e4>.form-switch</code>はスイッチスタイルのチェックボックスを表示します。</p><div class="form-check form-switch" data-v-05b676e4><input class="form-check-input" type="checkbox" id="switch_1" name="kyoka" value="yes" checked data-v-05b676e4><label class="form-check-label" for="switch_1" data-v-05b676e4>マイクやカメラへのアクセス許可</label></div>',3)),a("h5",le,[e[29]||(e[29]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[4]||(e[4]=l=>i(5))},s(f.value?"非表示":"表示"),1)]),f.value?(d(),n("pre",te,e[30]||(e[30]=[a("code",null,`<div class="form-check form-switch">
+  <input
+    class="form-check-input"
+    type="checkbox"
+    id="switch_1"
+    name="kyoka"
+    value="yes"
+    checked
+  />
+  <label class="form-check-label" for="switch_1">マイクやカメラへのアクセス許可</label>
+</div>
+`,-1)]))):c("",!0)])])]),a("div",ne,[a("div",de,[a("div",ie,[e[34]||(e[34]=r('<h2 class="mb-2" data-v-05b676e4>インラインチェック＆ラジオ(Inline check &amp; radio)</h2><p class="" data-v-05b676e4><code data-v-05b676e4>.form-check-inline</code>を指定すると、チェックボックスやラジオボタンを横並びに表示します。 </p><div data-v-05b676e4><div class="form-check form-check-inline" data-v-05b676e4><input class="form-check-input" type="checkbox" id="agree_info" name="agree[]" value="info" data-v-05b676e4><label class="form-check-label" for="agree_info" data-v-05b676e4>個人情報の取扱いに同意します</label></div><div class="form-check form-check-inline" data-v-05b676e4><input class="form-check-input" type="checkbox" id="agree_mail" name="agree[]" value="mail" data-v-05b676e4><label class="form-check-label" for="agree_mail" data-v-05b676e4>広告メールの受信に同意します</label></div></div><div class="mt-3" data-v-05b676e4><div class="form-check form-check-inline" data-v-05b676e4><input class="form-check-input" type="radio" id="radio_3" name="gender2" value="male2" checked data-v-05b676e4><label class="form-check-label" for="radio_3" data-v-05b676e4>男性</label></div><div class="form-check form-check-inline" data-v-05b676e4><input class="form-check-input" type="radio" id="radio_4" name="gender2" value="female2" data-v-05b676e4><label class="form-check-label" for="radio_4" data-v-05b676e4>女性</label></div></div>',4)),a("h5",ce,[e[32]||(e[32]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[5]||(e[5]=l=>i(6))},s(p.value?"非表示":"表示"),1)]),p.value?(d(),n("pre",re,e[33]||(e[33]=[a("code",null,`<div class="form-check form-check-inline">
+  <input
+    class="form-check-input"
+    type="checkbox"
+    id="agree_info"
+    name="agree[]"
+    value="info"
+  />
+  <label class="form-check-label" for="agree_info">個人情報の取扱いに同意します</label>
+</div>
+<div class="form-check form-check-inline">
+  <input
+    class="form-check-input"
+    type="checkbox"
+    id="agree_mail"
+    name="agree[]"
+    value="mail"
+  />
+  <label class="form-check-label" for="agree_mail">広告メールの受信に同意します</label>
+</div>
+
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" id="radio_3" name="gender" value="male" />
+  <label class="form-check-label" for="radio_3">男性</label>
+</div>
+<div class="form-check form-check-inline">
+  <input
+    class="form-check-input"
+    type="radio"
+    id="radio_4"
+    name="gender"
+    value="female"
+  />
+  <label class="form-check-label" for="radio_4">女性</label>
+</div>
+`,-1)]))):c("",!0)])])]),a("div",me,[a("div",be,[a("div",ve,[e[38]||(e[38]=a("h2",{class:"mb-2"},"レンジ(Range)",-1)),e[39]||(e[39]=a("p",{class:""},[a("code",null,".form-range"),o(" はレンジを表示します。minとmaxで最小値と最大値を設定できます。 ")],-1)),a("div",ue,[e[35]||(e[35]=a("label",{class:"form-label"},"例",-1)),O(a("input",{class:"form-range",type:"range",id:"range_1","onUpdate:modelValue":e[6]||(e[6]=l=>w.value=l),min:"0",max:"100"},null,512),[[B,w.value]]),a("div",null,"現在の値: "+s(w.value),1)]),a("h5",fe,[e[36]||(e[36]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[7]||(e[7]=l=>i(7))},s(k.value?"非表示":"表示"),1)]),k.value?(d(),n("pre",pe,e[37]||(e[37]=[a("code",null,`<label class="form-label">例</label>
+<input class="form-range" type="range" id="range_1" />
+`,-1)]))):c("",!0)])])]),a("div",ke,[a("div",he,[a("div",ye,[e[42]||(e[42]=r('<h2 class="mb-2" data-v-05b676e4>フローティングラベル(Floating labels)</h2><p class="" data-v-05b676e4><code data-v-05b676e4>.form-floating</code> は、ラベルを入力欄の内側に表示します。入力中は上部に小さく表示されます。<code data-v-05b676e4>input</code>は最初に記述する必要があり、<code data-v-05b676e4>placeholder</code>属性が必須です。 </p><div class="form-floating" data-v-05b676e4><input class="form-control" type="email" id="floating_label_1" placeholder="name@example.com" data-v-05b676e4><label for="floating_label_1" data-v-05b676e4>Your Name</label></div>',3)),a("h5",xe,[e[40]||(e[40]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[8]||(e[8]=l=>i(8))},s(h.value?"非表示":"表示"),1)]),h.value?(d(),n("pre",ge,e[41]||(e[41]=[a("code",null,`<div class="form-floating">
+  <input
+    class="form-control"
+    type="email"
+    id="floating_label_1"
+    placeholder="name@example.com"
+  />
+  <label for="floating_label_1">Your Name</label>
+</div>
+`,-1)]))):c("",!0)])])]),a("div",_e,[a("div",we,[a("div",Ce,[e[44]||(e[44]=a("h2",{class:"mb-2"},"チェックボックスとトグルボタン(Checkbox toggle buttons)",-1)),e[45]||(e[45]=a("p",{class:""},[a("code",null,".btn-check"),o(" を指定するとチェックボックスをトグルボタンとして利用することができます。"),a("br"),o("しかし、Vue3の場合"),a("code",null,"@click"),o("を使用して簡単に書けます。 この画面のコードの表示/非表示に使われています。 ")],-1)),a("button",{class:"btn btn-primary",style:{width:"5rem"},onClick:e[9]||(e[9]=l=>_.value=!_.value)},s(_.value?"On":"Off"),1),a("h5",Ee,[e[43]||(e[43]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[10]||(e[10]=l=>i(9))},s(y.value?"非表示":"表示"),1)]),y.value?(d(),n("pre",$e,[a("code",null,`<button class="btn btn-primary" style="width: 5rem" @click="isOn = !isOn">
+  `+s(_.value?"On":"Off")+`
+</button>
+`,1)])):c("",!0)])])]),e[53]||(e[53]=a("h1",{class:"mb-2 ms-3"},"レイアウト(Layout)",-1)),a("div",Ne,[a("div",Ve,[a("div",Fe,[e[48]||(e[48]=r('<h2 class="mb-2" data-v-05b676e4>フォームの横配置(Horizontal form)</h2><p class="" data-v-05b676e4> 横並びにしたい場合は<code data-v-05b676e4>.row</code>や<code data-v-05b676e4>.col</code>を使用します。<code data-v-05b676e4>.col-sm</code>などを利用すると、広い画面では横並びに、スマホなどの狭い画面では縦並びにすることもできます。 </p><div class="row" data-v-05b676e4><div class="col-sm" data-v-05b676e4><label class="form-label" for="layoutExam2-name" data-v-05b676e4>Name</label><input type="text" class="form-control" id="layoutExam2-name" name="name" data-v-05b676e4></div><div class="col-sm" data-v-05b676e4><label class="form-label" for="layoutExam2-address" data-v-05b676e4>Address</label><input type="text" class="form-control" id="layoutExam2-address" name="addres" data-v-05b676e4></div></div>',3)),a("h5",Oe,[e[46]||(e[46]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[11]||(e[11]=l=>i(10))},s(x.value?"非表示":"表示"),1)]),x.value?(d(),n("pre",Be,e[47]||(e[47]=[a("code",null,`<div class="row">
+  <div class="col-sm">
+    <label class="form-label" for="layoutExam2-name">Name</label>
+    <input type="text" class="form-control" id="layoutExam2-name" name="name" />
+  </div>
+  <div class="col-sm">
+    <label class="form-label" for="layoutExam2-address">Address</label>
+    <input type="text" class="form-control" id="layoutExam2-address" name="address" />
+  </div>
+</div>
+`,-1)]))):c("",!0)])])]),a("div",Ae,[a("div",Se,[a("div",ze,[e[51]||(e[51]=r('<h2 class="mb-2" data-v-05b676e4>ラベルの横配置(Horizontal label)</h2><p class="" data-v-05b676e4> ラベルも横並びにしたい場合にも<code data-v-05b676e4>.row</code>や<code data-v-05b676e4>.col</code>を使用します。<code data-v-05b676e4>.col-sm-*</code>を使用することでスマホでは縦配置になります。 <br data-v-05b676e4>ラベルは <code data-v-05b676e4>.form-label</code> の代わりに <code data-v-05b676e4>.col-form-label</code> や <code data-v-05b676e4>.col-form-label-{sm|lg}</code> を使用することで、ラベルをコントロールの高さの中央に配置することができます。 </p><div class="row mb-3" data-v-05b676e4><label class="col-form-label col-sm-2" for="layoutExam3-name" data-v-05b676e4>Name</label><div class="col-sm-10" data-v-05b676e4><input type="text" class="form-control" id="layoutExam3-name" name="name" data-v-05b676e4></div></div><div class="row" data-v-05b676e4><label class="col-form-label col-sm-2" for="layoutExam3-address" data-v-05b676e4>Address</label><div class="col-sm-10" data-v-05b676e4><input type="text" class="form-control" id="layoutExam3-address" name="address" data-v-05b676e4></div></div>',4)),a("h5",De,[e[49]||(e[49]=o(" Code ")),a("button",{type:"button",class:"btn btn-sm btn-outline-info ms-2",onClick:e[12]||(e[12]=l=>i(11))},s(g.value?"非表示":"表示"),1)]),g.value?(d(),n("pre",He,e[50]||(e[50]=[a("code",null,`<div class="row mb-3">
+  <label class="col-form-label col-sm-2" for="layoutExam3-name">Name</label>
+  <div class="col-sm-10">
+    <input type="text" class="form-control" id="layoutExam3-name" name="name" />
+  </div>
+</div>
+<div class="row">
+  <label class="col-form-label col-sm-2" for="layoutExam3-address">Address</label>
+  <div class="col-sm-10">
+    <input type="text" class="form-control" id="layoutExam3-address" name="address" />
+  </div>
+</div>
+`,-1)]))):c("",!0)])])]),E(S,{cancelpage:"/bootmenu",okVisible:!1})],64)}}},Le=N(Ie,[["__scopeId","data-v-05b676e4"]]);export{Le as default};
